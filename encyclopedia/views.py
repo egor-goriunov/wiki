470d1f33b252eb6a904c2f6413f65e263e9de3f5
Andrey Bykov <andrey.m.bykov@gmail.com>
2021-10-31 09:20:49 +0100

    added entries and links to entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseNotFound
+import markdown2
 from . import util
-
+  
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def show_entry(request,entry):
+    entry_text = util.get_entry(entry)
+    if entry_text is None:
+        return HttpResponseNotFound(f"<h1>Article {entry} not found. </h1>")
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "entry_text": markdown2.markdown (entry_text)
+    })
---
+++ entries/Walchwil.md
@@ -0,0 +1,4 @@
+# Walchwil
+
+Walchwil is a Village in Switzerland (Kanton Zug)
+Very nice views and very many rich people live in this village.
